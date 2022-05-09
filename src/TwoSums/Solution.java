ca0485bdffe560bf0988d314937b0eee01c4e57f
antal <antaladam13@gmail.com>
2022-05-09 23:10:14 +0200

    asd

--- src/TwoSums/Solution.java
+++ src/TwoSums/Solution.java
@@ -5,15 +5,13 @@ import java.util.Arrays;
 public class Solution {
     public int[] twoSum(Integer[] nums, int target) {
         for (Integer num : nums) {
-            int newtarget = target - num;
-            if (Arrays.asList(nums).contains(newtarget)) {
+            if (Arrays.asList(nums).contains(target - num)) {
                 int[] solution = new int[2];
                 solution[0] = num;
-                solution[1] = newtarget;
+                solution[1] = target - num;
                 return solution;
             }
         }
-        int[] sosekell = new int[0];
-        return sosekell;
+        return new int[0];
     }
 }
