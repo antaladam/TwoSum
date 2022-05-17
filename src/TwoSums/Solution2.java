d57fa1bd3d65f83be07d987d6e42fbb53cb37565
antal <antaladam13@gmail.com>
2022-05-17 21:25:23 +0200

    asd

--- src/TwoSums/Solution2.java
+++ src/TwoSums/Solution2.java
@@ -4,7 +4,7 @@ public class Solution2 {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = (nums.length - 1); j > 0; j--){
-                if ((nums[i] + nums[j]) == target){
+                if ((nums[i] + nums[j]) == target && i != j){
                     int[] answer = new int[2];
                     answer[0] = i;
                     answer[1] = j;
